cdc4a840da399caee62c4ed495f0f63b21400380
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-25 12:09:37 +0100

    feat: add jobs from data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header"
+import Jobs from "./jobs/Jobs"
 
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Jobs/>
     </div>
   )
 }
---
+++ src/jobs/Jobs.jsx
@@ -0,0 +1,39 @@
+import data from "../data/data.json";
+import Button from "../button/Button";
+const Jobs = () => {
+  return (
+    <main>
+      <div>
+        {data.map((job) => (
+          <section key={job.id}>
+            <div>
+              <img src={job.logo} alt="" />
+              <div>
+                <div>
+                  <h4>{job.company}</h4>
+                  <p>{job.new ? "New" : null}</p>
+                  <p>{job.featured ? "Featured" : null}</p>
+                </div>
+                <h3>{job.position}</h3>
+                <div>
+                  <small>{job.postedAt}</small>
+                  <small>{job.contract}</small>
+                  <small>{job.location}</small>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Button value={job.role} />
+              <Button value={job.level} />
+              {job.languages.map((language) => (
+                <Button value={language} key={job.id} />
+              ))}
+            </div>
+          </section>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Jobs;
