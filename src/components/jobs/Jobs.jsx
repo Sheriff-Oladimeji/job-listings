4118ad3fa8d2869051e52307c762217c82c19479
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-25 16:51:22 +0100

    fix: change coditionals for featured and new jobs

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -15,8 +15,8 @@ const Jobs = () => {
               <div>
                 <div className="card-tags">
                   <h4>{job.company}</h4>
-                  <p className="new">{job.new ? "New!" : null}</p>
-                  <p className="featured">{job.featured ? "Featured" :null}</p>
+                  {job.new == true && <p className="new">New!</p>}
+                  {job.featured == true && <p className="featured">Featured</p>}
                 </div>
                 <h3>{job.position}</h3>
                 <div className="card-about">
