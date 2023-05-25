f4cf9bd4638c91f78add98eac33c792ebe98d6d1
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-25 16:43:01 +0100

    modify files

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -16,7 +16,7 @@ const Jobs = () => {
                 <div className="card-tags">
                   <h4>{job.company}</h4>
                   <p className="new">{job.new ? "New!" : null}</p>
-                  <p className="featured">{job.featured ? "Featured" : null}</p>
+                  <p className="featured">{job.featured ? "Featured" :null}</p>
                 </div>
                 <h3>{job.position}</h3>
                 <div className="card-about">
