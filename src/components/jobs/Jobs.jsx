723a20a8584bdf70e848d74633c69db8cdcca1b7
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-25 16:12:35 +0100

    feat: add styles to card elements

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -6,32 +6,34 @@ const Jobs = () => {
     <main>
       <div className="cards">
         {data.map((job) => (
-          <section key={job.id} className={`card ${job.new?"card-new":""}`}>
+          <section
+            key={job.id}
+            className={`card ${job.featured ? "card-new" : ""}`}
+          >
             <div className="card-info">
               <img src={job.logo} alt="" />
               <div>
-                <div>
+                <div className="card-tags">
                   <h4>{job.company}</h4>
                   <p>{job.new ? "New" : null}</p>
                   <p>{job.featured ? "Featured" : null}</p>
                 </div>
                 <h3>{job.position}</h3>
-                <div>
-                  <small>{job.postedAt}</small>
-                  <small>{job.contract}</small>
+                <div className="card-about">
+                  <small>{job.postedAt}</small>•<small>{job.contract}</small>•
                   <small>{job.location}</small>
                 </div>
               </div>
             </div>
-            <div>
+            <div className="buttons">
               <Button value={job.role} />
               <Button value={job.level} />
               {job.languages.map((language) => (
                 <Button value={language} key={job.id} />
               ))}
-                    {job.tools.map(tool => (
-                        <Button value={tool} key={job.id}/>
-                    ))}
+              {job.tools.map((tool) => (
+                <Button value={tool} key={job.id} />
+              ))}
             </div>
           </section>
         ))}
