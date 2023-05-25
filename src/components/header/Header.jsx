8444d0592beab143779c311514c59c7c66f492f6
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-25 11:35:14 +0100

    feat: add styles to header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+
+import "./Header.css"
+const Header = () => {
+    return (
+      <header className="container">
+        {/* <div className="filter-values">
+          <input type="text" />
+          <button>Clear</button>
+        </div> */}
+      </header>
+    );
+}
+
+export default Header
