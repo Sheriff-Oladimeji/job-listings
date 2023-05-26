0c4b6a4486165920497c19d0340eb38bb8ed22ad
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-26 16:29:50 +0100

    feat: import images from assets to index

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import photosnap from "./photosnap.svg"
+import manage from "./manage.svg"
+import account from "./account.svg"
+import myhome from "./myhome.svg"
+import loopStudios  from "./loop-studios.svg"
+import faceit from "./faceit.svg"
+import shortly from "./shortly.svg"
+import insure from "./insure.svg"
+import eyecam from "./eyecam-co.svg"
+import airFlter from "./the-air-filter-company.svg"
+export{photosnap, manage, account, myhome, loopStudios, faceit, shortly, insure, eyecam,airFlter}
