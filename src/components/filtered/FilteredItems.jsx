bfe65c617df6a8494866797be8429ba7e050e1e8
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-28 03:48:16 +0100

    modify files

--- src/components/filtered/FilteredItems.jsx
+++ src/components/filtered/FilteredItems.jsx
@@ -1,13 +1,13 @@
-import "./filteredItems.css";
+import "./FilteredItems.css"
 import { useSelector } from "react-redux";
 const FilteredItems = () => {
   const jobs = useSelector((state) => state.filter.myArray);
  
   return (
-    <div className="filter-values">
-          <div>
+    <div className="filter-container">
+          <div className="filter-values">
               {jobs.map((job, index) => (
-                  <div key={index}>
+                  <div key={index} className="item">
                   {job}
                   <button>X</button>
                   </div>
