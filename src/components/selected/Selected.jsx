cf6659f3ad068479b4d0dee7b35e1606aa6665d3
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-28 20:15:40 +0100

    feat: set selected component to display only when there is an element in it

--- src/components/selected/Selected.jsx
+++ src/components/selected/Selected.jsx
@@ -1,18 +1,24 @@
 import { clearFilter, removeFilter } from "../../features/filter";
 import "./Selected.css";
-import { useSelector , useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 const Selected = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   let jobs = useSelector((state) => state.filter.myArray);
-  console.log(jobs)
+  console.log(jobs);
 
   return (
-    <div className="filter-container">
+    <div className={jobs.length === 0 ?"hidden" :"filter-container"}>
       <div className="filter-values">
         {jobs.map((job, index) => (
           <div key={index} className="item">
             <p>{job}</p>
-            <button onClick={(event) => dispatch(removeFilter(event.target.previousSibling.innerText))}>X</button>
+            <button
+              onClick={(event) =>
+                dispatch(removeFilter(event.target.previousSibling.innerText))
+              }
+            >
+              X
+            </button>
           </div>
         ))}
       </div>
